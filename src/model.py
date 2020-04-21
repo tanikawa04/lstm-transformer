015e17f90330b4ce73950aa663076e68e9b33021
tanikawa04 <tanikawa04@gmail.com>
2020-04-22 00:41:49 +0900

    LSTMTransformerModel を追加

--- src/model.py
+++ src/model.py
@@ -1,3 +1,4 @@
+import copy
 import math
 import torch
 import torch.nn as nn
@@ -150,3 +151,167 @@ class TransformerModel(nn.Module):
         output = self.transformer_encoder(src, self.src_mask)
         output = self.decoder(output)
         return F.log_softmax(output, dim=-1)
+
+class MemTransformerModule(nn.Module):
+    r"""MemTransformerModule is a stack of N decoder layers"""
+    __constants__ = ['norm']
+
+    def __init__(self, decoder_layer, num_layers, norm=None):
+        super(MemTransformerModule, self).__init__()
+        self.layers = _get_clones(decoder_layer, num_layers)
+        self.num_layers = num_layers
+        self.norm = norm
+
+    def forward(self, tgt, mems=None, memory_mask=None,
+                memory_key_padding_mask=None):
+        r"""Pass the inputs (and mask) through the decoder layer in turn."""
+        output = tgt
+        if mems is None:
+            device = tgt.device
+            mems = [torch.empty(0).to(device) for _ in range(self.num_layers)]
+        mems2 = []
+
+        for layer, memory in zip(self.layers, mems):
+            output, memory = layer(output, memory, memory_mask=memory_mask,
+                                   memory_key_padding_mask=memory_key_padding_mask)
+            mems2.append(memory)
+
+        if self.norm is not None:
+            output = self.norm(output)
+
+        return output, mems2
+
+class MemTransformerLayer(nn.Module):
+    r"""MemTransformerLayer is made up of self-attn, multi-head-attn and feedforward network."""
+
+    def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1, activation="relu"):
+        super(MemTransformerLayer, self).__init__()
+        self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)
+        # Implementation of Feedforward model
+        self.linear1 = nn.Linear(d_model, dim_feedforward)
+        self.dropout = nn.Dropout(dropout)
+        self.linear2 = nn.Linear(dim_feedforward, d_model)
+
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+
+        self.activation = _get_activation_fn(activation)
+
+    def __setstate__(self, state):
+        if 'activation' not in state:
+            state['activation'] = F.relu
+        super(MemTransformerLayer, self).__setstate__(state)
+
+    def forward(self, tgt, memory, memory_mask=None,
+                memory_key_padding_mask=None):
+        r"""Pass the inputs (and mask) through the decoder layer."""
+        memory = torch.cat((memory, tgt), dim=0)
+        tgt2 = self.self_attn(tgt, memory, memory, attn_mask=memory_mask,
+                              key_padding_mask=memory_key_padding_mask)[0]
+        tgt = tgt + self.dropout1(tgt2)
+        tgt = self.norm1(tgt)
+        tgt2 = self.linear2(self.dropout(self.activation(self.linear1(tgt))))
+        tgt = tgt + self.dropout2(tgt2)
+        tgt = self.norm2(tgt)
+        return tgt, memory
+
+class LSTMTransformerModel(nn.Module):
+    """Container module with an encoder, a LSTM module, a transformer module, and a decoder."""
+
+    def __init__(self, ntoken, ninp, nhead, nhid, nlayers, dropout=0.5):
+        super(LSTMTransformerModel, self).__init__()
+        try:
+            from torch.nn import TransformerEncoder, TransformerEncoderLayer
+        except:
+            raise ImportError('TransformerEncoder module does not exist in PyTorch 1.1 or lower.')
+        self.model_type = 'LSTMTransformer'
+        self.src_mask = None
+        self.nlstmlayers = 1
+        self.lstm_encoder = nn.LSTM(ninp, ninp, self.nlstmlayers)
+        transformer_layer = MemTransformerLayer(ninp, nhead, nhid, dropout)
+        self.transformer_encoder = MemTransformerModule(transformer_layer, nlayers)
+        self.encoder = nn.Embedding(ntoken, ninp)
+        self.ninp = ninp
+        self.decoder = nn.Linear(ninp, ntoken)
+
+        self.init_weights()
+
+    def _generate_square_subsequent_mask(self, sz):
+        mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
+        mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
+        return mask
+
+    def init_weights(self):
+        initrange = 0.1
+        self.encoder.weight.data.uniform_(-initrange, initrange)
+        self.decoder.bias.data.zero_()
+        self.decoder.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, src, hidden=None, mems=None, has_mask=True):
+        if has_mask:
+            device = src.device
+            src_len = len(src)
+            mem_len = len(mems[0]) if mems is not None else 0
+            if self.src_mask is None or self.src_mask.size(0) != src_len + mem_len:
+                mask = self._generate_square_subsequent_mask(src_len).to(device)
+                if mem_len > 0:
+                    mem_mask = torch.zeros((src_len, mem_len)).to(device)
+                    mask = torch.cat((mem_mask, mask), dim=1)
+                self.src_mask = mask
+        else:
+            self.src_mask = None
+
+        src = self.encoder(src) * math.sqrt(self.ninp)
+        src, hidden = self.lstm_encoder(src, hidden)
+        output, mems = self.transformer_encoder(src, mems=mems, memory_mask=self.src_mask)
+        output = self.decoder(output)
+        return F.log_softmax(output, dim=-1), hidden, mems
+
+    def init_hidden(self, bsz):
+        weight = next(self.parameters())
+        return (weight.new_zeros(self.nlstmlayers, bsz, self.ninp),
+                weight.new_zeros(self.nlstmlayers, bsz, self.ninp))
+
+def _get_clones(module, N):
+    return nn.ModuleList([copy.deepcopy(module) for i in range(N)])
+
+def _get_activation_fn(activation):
+    if activation == "relu":
+        return F.relu
+    elif activation == "gelu":
+        return F.gelu
+
+    raise RuntimeError("activation should be relu/gelu, not {}".format(activation))
+
+
+if __name__ == "__main__":
+    # sample code
+    ntoken = 100
+    ninp = 10
+    nhead = 2
+    nhid = 10
+    nlayers = 2
+    seq_len = 5
+    batch_size = 4
+
+    model = LSTMTransformerModel(ntoken, ninp, nhead, nhid, nlayers)
+
+    data = torch.randint(0, ntoken, (seq_len, batch_size))
+    hidden = model.init_hidden(batch_size)
+    mems = None
+
+    logit, hidden, mems = model(data, hidden=hidden, mems=mems)
+
+    print('logit:')
+    print(logit)
+    print(logit.size())
+    print()
+    print('hidden:')
+    print(hidden)
+    print(hidden[0].size(), hidden[1].size())
+    print()
+    print('mems:')
+    print(mems)
+    print([mem.size() for mem in mems])
